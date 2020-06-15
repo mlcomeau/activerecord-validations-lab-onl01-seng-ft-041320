eaddc0bea06b54d7cb536977da36f262c7a91dbf
Megan Comeau <mlcomeau99@gmail.com>
2020-06-15 22:39:28 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -1,7 +1,11 @@
 class ClickbaitValidator < ActiveModel::Validator
   def validate(record)
-    if !record.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
-      record.errors[:title] << "Invalid Title"
-    end
+    if data.title.present?
+      if !data.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+        data.errors[:title] << "Invalid Title"
+      end
+    else
+       data.errors[:title] << "Title can't be blank"
+   end
   end
 end
