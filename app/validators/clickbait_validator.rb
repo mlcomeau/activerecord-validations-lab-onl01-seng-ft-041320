6583825707b7022f32850eefc898451f9758e96f
Megan Comeau <mlcomeau99@gmail.com>
2020-06-15 22:38:07 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -1,7 +1,7 @@
 class ClickbaitValidator < ActiveModel::Validator
   def validate(record)
-    if !data.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
-      data.errors[:title] << "Invalid Title"
+    if !record.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+      record.errors[:title] << "Invalid Title"
     end
   end
 end
