14d80235944fe4561ffd3b809f2ee28500648ae6
Megan Comeau <mlcomeau99@gmail.com>
2020-06-15 22:34:52 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  #validates_with ClickbaitValidator
+  validates_with ClickbaitValidator
 end
